0c6216c7a3ab116dad7769260a4dc9f19fae7fce
Piotr Piwoński <piwonskp@gmail.com>
2021-07-26 19:10:28 +0200

    Use chainID isntead of signer

--- core/types/fuzz_utils.go
+++ core/types/fuzz_utils.go
@@ -4,12 +4,12 @@ import (
 	"math/big"
 )
 
-func SetSignatureValues(tx *Transaction, signer Signer, v, r, s *big.Int) (*Transaction) {
+func SetSignatureValues(tx *Transaction, chainID, v, r, s *big.Int) (*Transaction) {
 	// Copied from WithSignature func
 	// https://github.com/ethereum/go-ethereum/blob/c503f98f6d5e80e079c1d8a3601d188af2a899da/core/types/transaction.go#L397
 
 	cpy := tx.inner.copy()
-	cpy.setSignatureValues(signer.ChainID(), v, r, s)
+	cpy.setSignatureValues(chainID, v, r, s)
 	return &Transaction{inner: cpy, time: tx.time}
 }
 
